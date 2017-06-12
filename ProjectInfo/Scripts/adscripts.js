b0ecf4a92d3c559b08cf2121497e81b9c126ee8a
dpetryk <damian.petryk@gmail.com>
2017-06-12 22:35:14 +0200

    kilka zmian, głównie style

--- ProjectInfo/Scripts/adscripts.js
+++ ProjectInfo/Scripts/adscripts.js
@@ -1,35 +1,33 @@
 ﻿
 function colorizeDetails() {
-    $("#status:contains('Completed')").css({"color" : "green", "font-weight": "Bold"});
-    $("#status:contains('progres')").css({"color": "black", "font-weight": "Bold"});
+    $("#status:contains('Completed')").css({ "color": "#00ff21", "font-weight": "Bold" });
 
     $("#priority:contains('High')").css({"color" : "red", "font-weight": "Bold"});
-    $("#priority:contains('Low')").css({"color" : "green", "font-weight": "Bold"});
+    $("#priority:contains('Low')").css({ "color": "#02c873", "font-weight": "Bold" });
     $("#priority:contains('Medium')").css({ "color": "orange", "font-weight": "Bold" });
 
     $("#effort:contains('Big')").css({"color": "red", "font-weight": "Bold"});
-    $("#effort:contains('Small')").css({"color": "green", "font-weight": "Bold"});
+    $("#effort:contains('Small')").css({ "color": "#02c873", "font-weight": "Bold" });
     $("#effort:contains('Medium')").css({"color": "orange", "font-weight": "Bold"});
 
     $("#dstatus:contains('Red')").css({"color": "red", "font-weight": "Bold"});
-    $("#dstatus:contains('Green')").css({"color": "green", "font-weight": "Bold"});
+    $("#dstatus:contains('Green')").css({ "color": "#02c873", "font-weight": "Bold" });
     $("#dstatus:contains('Amber')").css({ "color": "orange", "font-weight": "Bold" });
 };
 
 function colorizeIndex() {
     $("tr td:nth-child(6):contains('Completed')").css({ "color": "green", "font-weight": "Bold" });
-    $("tr td:nth-child(6):contains('progres')").css({ "color": "black", "font-weight": "Bold" });
 
     $("tr td:nth-child(7):contains('High')").css({ "color": "red", "font-weight": "Bold" });
-    $("tr td:nth-child(7):contains('Low')").css({ "color": "green", "font-weight": "Bold" });
+    $("tr td:nth-child(7):contains('Low')").css({ "color": "#02c873", "font-weight": "Bold" });
     $("tr td:nth-child(7):contains('Medium')").css({ "color": "orange", "font-weight": "Bold" });
 
     $("tr td:nth-child(8):contains('Big')").css({ "color": "red", "font-weight": "Bold" });
-    $("tr td:nth-child(8):contains('Small')").css({ "color": "green", "font-weight": "Bold" });
+    $("tr td:nth-child(8):contains('Small')").css({ "color": "#02c873", "font-weight": "Bold" });
     $("tr td:nth-child(8):contains('Medium')").css({ "color": "orange", "font-weight": "Bold" });
 
     $("tr td:nth-child(9):contains('Red')").css({ "color": "red", "font-weight": "Bold" });
-    $("tr td:nth-child(9):contains('Green')").css({ "color": "green", "font-weight": "Bold" });
+    $("tr td:nth-child(9):contains('Green')").css({ "color": "#02c873", "font-weight": "Bold" });
     $("tr td:nth-child(9):contains('Amber')").css({ "color": "orange", "font-weight": "Bold" });
 };
 
@@ -71,28 +69,19 @@ var compare = {
         } else {                              
             return a > b ? 1 : 0;               
         }                                    
+    },
+
+
+    date: function(a, b) {                 
+        a = new Date(a);                     
+        b = new Date(b);                     
+
+        return a - b;                        
     }
-
-
-    //duration: function(a, b) {             
-    //    a = a.split(':');                    
-    //    b = b.split(':');                    
-
-    //    a = Number(a[0]) * 60 + Number(a[1]);
-    //    b = Number(b[0]) * 60 + Number(b[1]); 
-
-    //    return a - b;                         
-    //},
-    //date: function(a, b) {                 
-    //    a4 = new Date(a);                     
-    //    b = new Date(b);                     
-
-    //    return a - b;                        
-    //}
 
 };
 
-$('.sortable').each(function () {
+$('.table-hover').each(function () {
     var $table = $(this);
     var $tbody = $table.find('tbody');        
     var $controls = $table.find('th');
@@ -124,4 +113,8 @@ $('.sortable').each(function () {
                 }
             }
         });
-    });
+});
+
+//$("#projectTable>tbody>tr").on('click-row.bs.table', function (e, row, $element) {
+//    window.location = $element.data('href');
+//});
